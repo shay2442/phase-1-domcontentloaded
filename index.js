167b540372e900641593e43c48347115a3e24d70
Shay Wacker <shay2442@yahoo.com>
2021-08-25 16:51:16 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log(document.getElementById('text').textContent = 'This is really cool!');
+  });
+  
+  console.log(document.getElementById('text').textContent = 'JavaScript is so cool. It lets me add text to my page programmatically.'
+    
+  );
+
+
+
+//   document.addEventListener("DOMContentLoaded", function() {
+//     console.log("The DOM has loaded");
+//   });
+  
+//   console.log(
+//     "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+//   );
